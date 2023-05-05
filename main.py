53f9dcbdc0634733a24b162082834f74a5d8fb8b
Santiago Miranda <santiago.miranda.rodriguez21@gmail.com>
2023-05-05 11:39:15 +1000

    password VARCHAR (128)

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
     posts = relationship("BlogPost", back_populates="author")
     comments = relationship("Comments", back_populates="author")
 
